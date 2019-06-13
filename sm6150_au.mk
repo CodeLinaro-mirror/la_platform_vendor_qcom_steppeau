02a04a1d5305adbc2ce7fffa1b85e77c1283346d
Vijayakumar Badiger <vbadig@codeaurora.org>
2019-06-12 18:27:08 -0700

    automotive bringup changes
    Change-Id: Ie0f921209c863f1007599db84c44eeb2422b6d0c

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -103,13 +103,14 @@ PRODUCT_COPY_FILES += hardware/qcom/media/conf_files/msmnile/system_properties.x
 PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
 
 # Audio configuration file
--include $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/msmsteppe_au/msmsteppe_au.mk
+-include $(TOPDIR)hardware/qcom/audio/configs/msmsteppe_au/msmsteppe_au.mk
 
 #Audio DLKM
 AUDIO_DLKM := audio_apr.ko
 #AUDIO_DLKM += audio_snd_event.ko
 AUDIO_DLKM += audio_q6_notifier.ko
 AUDIO_DLKM += audio_adsp_loader.ko
+AUDIO_DLKM += audio_snd_event.ko
 AUDIO_DLKM += audio_q6.ko
 AUDIO_DLKM += audio_platform.ko
 AUDIO_DLKM += audio_hdmi.ko
@@ -119,7 +120,7 @@ AUDIO_DLKM += audio_machine_talos.ko
 PRODUCT_PACKAGES += $(AUDIO_DLKM)
 
 PRODUCT_PACKAGES += fs_config_files
-ENABLE_AB ?= false
+ENABLE_AB ?= true
 
 ifeq ($(ENABLE_AB), true)
 #A/B related packages
