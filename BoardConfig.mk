c06c4054ee20b185c6b54bda6c0ae9070f655840
Hans Chang <hansc@codeaurora.org>
2019-06-17 10:04:34 -0700

    added emac_dwc_eqos in Talos.
    Change-Id: I25422aa3f76ef85d2ad79f6bac738682f43e97bb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,9 +112,8 @@ BOARD_VENDOR_KERNEL_MODULES := \
     $(KERNEL_MODULES_OUT)/audio_native.ko \
     $(KERNEL_MODULES_OUT)/audio_machine_talos.ko \
     $(KERNEL_MODULES_OUT)/wil6210.ko \
-    $(KERNEL_MODULES_OUT)/msm_11ad_proxy.ko
-    #$(KERNEL_MODULES_OUT)/emac_dwc_eqos.ko \
-    $(KERNEL_MODULES_OUT)/rdbg.ko
+    $(KERNEL_MODULES_OUT)/msm_11ad_proxy.ko \
+    $(KERNEL_MODULES_OUT)/emac_dwc_eqos.ko
 
 # install lkdtm only for userdebug and eng build variants
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
