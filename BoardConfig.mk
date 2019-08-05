fb3eb8676a29ec24ef56b1d97b85dc9d14096587
Abhiprayah Tiwari <abhipray@codeaurora.org>
2019-08-05 16:14:11 +0530

    sm6150_au: Add support for perf defconfig in user build
    Update the mk files to pick perf defconfig when building for
    user build.
    
    Change-Id: I4027d3aaf55f109ad2bdb713bc2deaf8e7e672ab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,7 +97,11 @@ BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 # Compile Linux Kernel
 #----------------------------------------------------------------------
 ifeq ($(KERNEL_DEFCONFIG),)
-     KERNEL_DEFCONFIG := $(shell ls ./kernel/msm-4.14/arch/arm64/configs/vendor/ | grep sdmsteppe-auto_defconfig)
+    ifeq ($(TARGET_BUILD_VARIANT),user)
+        KERNEL_DEFCONFIG := sdmsteppe-auto-perf_defconfig
+    else
+        KERNEL_DEFCONFIG := sdmsteppe-auto_defconfig
+    endif
 endif
 
 BOARD_VENDOR_KERNEL_MODULES := \
