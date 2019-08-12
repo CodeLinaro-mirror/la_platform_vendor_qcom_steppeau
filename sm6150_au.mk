934c65550e7d6fac99c8bb2d96324b0127f7d07f
Frank Liu <qiliu@codeaurora.org>
2019-08-12 16:17:14 +0800

    sm6150_au: add genoa for multiple wlan drivers
    Add 'genoa' into TARGET_WLAN_CHIP to indicate genoa will be
    supported also.
    
    Change-Id: Iabf2d974aa67190dd33fe061537e374ad569060b
    CRs-Fixed: 2502011

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -225,7 +225,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # wlan specific
 #----------------------------------------------------------------------
 # Multiple chips
-TARGET_WLAN_CHIP := qca6174 qca6390
+TARGET_WLAN_CHIP := qca6174 qca6390 qcn7605
 include device/qcom/wlan/$(PRODUCT_NAME)/wlan.mk
 
 
