f0b3b526423e093b622e8cd7864fa79cb9180c63
Singa Reddy Dasari <singredd@codeaurora.org>
2019-12-17 12:24:15 +0530

    Removed build of  Context Hub HAL and service for automotive Talos
    Acked By: Abhinay Vundyala

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -183,10 +183,7 @@ PRODUCT_COPY_FILES += device/qcom/$(MSMSTEPPE)/msm_irqbalance.conf:$(TARGET_COPY
 
 
 
-# Context hub HAL
-PRODUCT_PACKAGES += \
-    android.hardware.contexthub@1.0-impl.generic \
-    android.hardware.contexthub@1.0-service
+
 
 # MIDI feature
 PRODUCT_COPY_FILES += \
