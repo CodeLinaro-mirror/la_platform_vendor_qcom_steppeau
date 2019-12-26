c9235b3918623421ce4dc3840f7b3fd18de2b7e8
Chennuru Rajeshwar Reddy <rchenn@codeaurora.org>
2019-12-26 16:22:15 +0530

    Added TARGET_USES_GAS Flag
    Change-Id: Ied3b17e6b9c0eeccc774db4b7235eb3c1f45eea7

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -20,6 +20,7 @@ TARGET_ENABLE_QC_AV_ENHANCEMENTS := false
 TARGET_USES_AOSP_FOR_WLAN := false
 ENABLE_CAR_POWER_MANAGER := true
 ENABLE_MODEM_DATA := true
+TARGET_USES_GAS := true
 
 # Dynamic-partition enabled by default
 BOARD_DYNAMIC_PARTITION_ENABLE := false
