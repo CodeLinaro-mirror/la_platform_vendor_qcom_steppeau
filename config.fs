3dd8ec5abf358c3009c7e9e7918ba37c19737a0c
Pankaj Kumar <pankkuma@codeaurora.org>
2020-05-05 11:59:31 +0530

    sm6150_au: Added early service support
    Change-Id: Iccd0287568547267c4756171a1af9727cb3c5319

---
+++ config.fs
@@ -0,0 +1,41 @@
+[early_services/]
+mode: 0755
+user: AID_ROOT
+group: AID_SHELL
+caps: 0
+
+[early_services/system/bin/]
+mode: 0751
+user: AID_ROOT
+group: AID_SHELL
+caps: 0
+
+[early_services/vendor/bin/]
+mode: 0751
+user: AID_ROOT
+group: AID_SHELL
+caps: 0
+
+[early_services/system/bin/*]
+mode: 0755
+user: AID_ROOT
+group: AID_SHELL
+caps: 0
+
+[early_services/vendor/bin/*]
+mode: 0755
+user: AID_ROOT
+group: AID_SHELL
+caps: 0
+
+[early_services/init_early]
+mode: 0750
+user: AID_ROOT
+group: AID_SHELL
+caps: 0
+
+[early_services/init_early_test]
+mode: 0750
+user: AID_ROOT
+group: AID_SHELL
+caps: 0
