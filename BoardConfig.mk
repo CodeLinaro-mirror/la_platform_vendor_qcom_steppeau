8f091a7105c35263025156bf914e4264ff07140a
Ramesh Kodam <rameko@codeaurora.org>
2020-06-30 17:26:39 -0700

    sm6150: Use sepolicy_vndr directory
    Use sepolicy_vndr directory for generating sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -257,5 +257,5 @@ BUILD_BROKEN_USES_BUILD_HOST_STATIC_LIBRARY := true
 -include vendor/qcom/defs/board-defs/system/*.mk
 -include vendor/qcom/defs/board-defs/vendor/*.mk
 #################################################################################
-include device/qcom/sepolicy/SEPolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 
