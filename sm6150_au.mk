51c8779c1102ec768efcf5d20f878b0ae157e548
Santosh Dronamraju <sdronamr@codeaurora.org>
2020-08-24 18:15:14 +0530

    sm6150_au: Support ufs and emmc variant fstab files
    Two fstabs are needed to support different data unit
    number (dun) size for ufs and emmc hardwares. This
    needs different encryption flag in fstab to address
    the difference in dun size.
    
    Change-Id: I64c64b7298dd3549bc44deed44da0073fb930b31

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -46,9 +46,11 @@ ifeq ($(strip $(BOARD_DYNAMIC_PARTITION_ENABLE)),true)
   PRODUCT_BUILD_USERDATA_IMAGE := true
 
   ifeq ($(ENABLE_AB), true)
-    PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/default/fstab_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/emmc/fstab_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.emmc
   else
-    PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_non_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/default/fstab_non_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/emmc/fstab_non_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.emmc
   endif
 endif #BOARD_DYNAMIC_PARTITION_ENABLE
 
