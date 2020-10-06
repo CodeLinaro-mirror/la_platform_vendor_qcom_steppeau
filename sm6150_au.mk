4e213730f97191714562a383e6c20fcb8070aaf6
Swapneel Mashalkar <smashalk@codeaurora.org>
2020-10-06 12:16:11 +0530

    [Sm6150_AU]: Bootcontrol HAL support in recovery.
    Added Bootctl hal support in recovery, this will be
    helpful in flashing A/B images and slot switching.
    
    Change-Id: I15381a3257d2b23d53384642d08aa61bc845077e

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -170,8 +170,12 @@ PRODUCT_PACKAGES += update_engine \
     update_engine_client \
     update_verifier \
     bootctrl.$(MSMSTEPPE) \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service
+
+PRODUCT_PACKAGES += \
+    update_engine_sideload
 
 PRODUCT_HOST_PACKAGES += \
 	brillo_update_payload
