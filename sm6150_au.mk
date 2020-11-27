cc1e7627e0fc8e504457ca2c63f680a1ef76cef8
Abhiprayah Tiwari <abhipray@codeaurora.org>
2020-11-27 19:32:45 +0530

    sm6150_au: Adding MTMD mapping and settings
    Adding mappings for MTMD and settings to support Multi IME,
    system decorations etc.
    
    Change-Id: Ifd587a6897b2a2dec8948ca7714b08bc176bf8bd

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -190,7 +190,8 @@ PRODUCT_PACKAGES += \
 
 # MTMD enablement
 PRODUCT_COPY_FILES += \
-    device/qcom/sm6150_au/input-port-associations.xml:$(TARGET_COPY_OUT_VENDOR)/etc/input-port-associations.xml
+    device/qcom/sm6150_au/input-port-associations.xml:$(TARGET_COPY_OUT_VENDOR)/etc/input-port-associations.xml \
+    device/qcom/sm6150_au/display_settings.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display_settings.xml
 
 
 DEVICE_MANIFEST_FILE := device/qcom/sm6150_au/manifest.xml
