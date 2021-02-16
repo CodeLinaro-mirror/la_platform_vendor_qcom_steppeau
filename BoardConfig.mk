16a63175327d27eeaca1a8c899e1b4b42cc4b07b
Venkata Rao Kakani <vkakani@codeaurora.org>
2021-02-16 14:34:04 +0530

    copy gen_headers_auto to gen_headers.
    Change-Id: I1e6dbdddee932185e739b4e07b2dce06a322503d
    Signed-off-by: Venkata Rao Kakani <vkakani@codeaurora.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,9 +181,9 @@ else
 endif
 
 KERN_PATH := kernel/msm-5.4/
-$(shell rm $(KERN_PATH)gen_headers_arm64.bp $(KERN_PATH)gen_headers_arm.bp)
-$(shell ln $(KERN_PATH)gen_headers_arm64_auto.bp $(KERN_PATH)gen_headers_arm64.bp)
-$(shell ln $(KERN_PATH)gen_headers_arm_auto.bp $(KERN_PATH)gen_headers_arm.bp)
+$(shell rm -f $(KERN_PATH)gen_headers_arm64.bp $(KERN_PATH)gen_headers_arm.bp)
+$(shell cp $(KERN_PATH)gen_headers_arm64_auto.bp $(KERN_PATH)gen_headers_arm64.bp)
+$(shell cp $(KERN_PATH)gen_headers_arm_auto.bp $(KERN_PATH)gen_headers_arm.bp)
 
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
 MAX_EGL_CACHE_SIZE := 2048*1024
