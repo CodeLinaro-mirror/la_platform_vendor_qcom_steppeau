5ccff12dc59b01e38ef5898d6d85f827706ec7fe
Jayadev K <jayak@codeaurora.org>
2021-05-04 18:27:24 +0530

    sm6150_au: Uncomment hsi2s driver module
    Change-Id: I1fee6aae0b5bc7e71b0da6f6c5efb433721ea6de

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,7 +138,8 @@ ifeq ($(KERNEL_DEFCONFIG),)
     endif
 endif
 
-#BOARD_VENDOR_KERNEL_MODULES := \
+BOARD_VENDOR_KERNEL_MODULES := \
+     $(KERNEL_MODULES_OUT)/hsi2s.ko
 #    $(KERNEL_MODULES_OUT)/audio_apr.ko \
 #    $(KERNEL_MODULES_OUT)/audio_snd_event.ko \
 #    $(KERNEL_MODULES_OUT)/audio_q6_notifier.ko \
@@ -152,7 +153,6 @@ endif
 #    $(KERNEL_MODULES_OUT)/wil6210.ko \
 #    $(KERNEL_MODULES_OUT)/msm_11ad_proxy.ko \
 #    $(KERNEL_MODULES_OUT)/emac_dwc_eqos.ko \
-#    $(KERNEL_MODULES_OUT)/hsi2s.ko
 
 # install lkdtm only for userdebug and eng build variants
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
