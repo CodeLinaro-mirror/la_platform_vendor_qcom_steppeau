2eb652265a4f02d858206aa511a5c25793e545da
Kalyan Rao D <kalyand@codeaurora.org>
2021-06-10 22:39:46 +0530

    Changes for SA8155P.LA.1.3 • FR69231:Remove Telephony configuration makefiles from automotive builds
    Change-Id: I2cf724ac9428f510fd370f00da60c99b20a545d7

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -117,8 +117,11 @@ $(warning ****** MSMSTEPPE code name is: $(MSMSTEPPE))
 #    device/qcom/$(MSMSTEPPE)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
 PRODUCT_BOOT_JARS += tcmiface
+
+ifneq ($(TARGET_NO_TELEPHONY), true)
 PRODUCT_BOOT_JARS += telephony-ext
 PRODUCT_PACKAGES += telephony-ext
+endif
 
 
 
