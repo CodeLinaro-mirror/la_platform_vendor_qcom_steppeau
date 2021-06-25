7500d9553306d9149abb1b65d96bf2fced5e1d9d
Vijayakumar Badiger <vbadig@codeaurora.org>
2021-06-25 14:34:39 -0700

    sm6150_au: add recovery for boot image header version 3 support
    Change-Id: Ieb2282f795fc5ea5002f8dba517ffeb844f2e3ce

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,18 +40,21 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/qcom/common/automotive
 USE_OPENGL_RENDERER := true
 BOARD_USE_LEGACY_UI := true
 
+#Generate DTBO image
+BOARD_KERNEL_SEPARATED_DTBO := true
+
 # Set Header version for bootimage
 #Disable appended dtb
 TARGET_KERNEL_APPEND_DTB := false
 
-# Set Header version for bootimage
-ifneq ($(strip $(TARGET_KERNEL_APPEND_DTB)),true)
-#Enable dtb in boot image and Set Header version
+#Enable dtb in boot image and boot image header version 3 support.
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_BOOT_HEADER_VERSION := 3
-else
-BOARD_BOOT_HEADER_VERSION := 1
+ifeq ($(ENABLE_AB), true)
+  BOARD_USES_RECOVERY_AS_BOOT := true
+  TARGET_NO_RECOVERY := true
 endif
+
+BOARD_BOOT_HEADER_VERSION := 3
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 TARGET_RECOVERY_PIXEL_FORMAT:= RGBX_8888
@@ -82,13 +85,13 @@ else
   BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
   BOARD_EXT4_SHARE_DUP_BLOCKS := true
   BOARD_USES_METADATA_PARTITION := true
-  ifeq ($(BOARD_KERNEL_SEPARATED_DTBO),true)
-    # Enable DTBO for recovery image
-    BOARD_INCLUDE_RECOVERY_DTBO := true
-  endif
 endif
 ### Dynamic partition Handling
 
+ifeq ($(BOARD_KERNEL_SEPARATED_DTBO),true)
+   # Enable DTBO for recovery image
+   BOARD_INCLUDE_RECOVERY_DTBO := true
+endif
 
 # Defines for enabling A/B builds
 AB_OTA_UPDATER := true
@@ -233,9 +236,6 @@ USE_SENSOR_MULTI_HAL := false
 
 #Add non-hlos files to ota packages
 ADD_RADIO_FILES := true
-
-#Generate DTBO image
-BOARD_KERNEL_SEPARATED_DTBO := true
 
 #Enable INTERACTION_BOOST
 TARGET_USES_INTERACTION_BOOST := true
