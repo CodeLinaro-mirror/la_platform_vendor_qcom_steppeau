17a43b98f287f48fa8e2fd7f82b6cc5304bfbbd9
Santosh Dronamraju <sdronamr@codeaurora.org>
2021-08-20 01:41:56 +0530

    sm6150_au:Fix for cts keystore attestation test
    Add keystore xml file to vendor path so that it will be available
    in pm list and it will get proper feature version.
    
    Change-Id: I27e8815bc2292d97c9dd70efedda8a0ccedf67a9

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -160,6 +160,7 @@ endif #TARGET_ENABLE_QC_AV_ENHANCEMENTS
 
 #PRODUCT_COPY_FILES += hardware/qcom/media/conf_files/msmnile/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
+PRODUCT_COPY_FILES += hardware/interfaces/security/keymint/aidl/default/android.hardware.hardware_keystore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.hardware_keystore.xml
 PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
 
 # Audio configuration file
