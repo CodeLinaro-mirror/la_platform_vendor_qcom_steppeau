e89a77248750f1cbd2ad1d89791c2001bda481be
Venkata Satyam Raju B <vboddepa@codeaurora.org>
2021-09-23 10:11:04 -0700

    Revert "enable system_ext partition"
    This reverts commit b67c1ded815ac338523fae0a7a333183029a8ece.
    
    Change-Id: I328aa2e25658c3644f660b6d8638000936ea958b

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -43,11 +43,10 @@ ifeq ($(strip $(BOARD_DYNAMIC_PARTITION_ENABLE)),true)
   BOARD_BUILD_SUPER_IMAGE_BY_DEFAULT := true
   PRODUCT_BUILD_SUPER_PARTITION := true
   PRODUCT_PACKAGES += fastbootd
-  # Enable System_ext
-  PRODUCT_BUILD_SYSTEM_EXT_IMAGE := true
+
 # Mismatch in the uses-library tags between build system and the manifest leads
 # to soong APK manifest_check tool errors. Enable the flag to fix this.
-  RELAX_USES_LIBRARY_CHECK := true
+RELAX_USES_LIBRARY_CHECK := true
 
   BOARD_AVB_VBMETA_SYSTEM := system
   BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
