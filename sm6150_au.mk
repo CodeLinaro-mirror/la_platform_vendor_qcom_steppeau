cc0bcd6b7b90b1f5a495bc388826ef87acb30b24
Rajesh Ramaiah Narayanaswamy <quic_rramai@quicinc.com>
2021-10-08 10:48:19 +0530

    Revert "sm6150_au : Enable GAS flag"
    This reverts commit e7599a2ccc98b5b7ef397c3a8943bdeb3a4c6323.
    
    Signed-off-by: Rajesh Ramaiah Narayanaswamy <quic_rramai@quicinc.com>

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -28,7 +28,7 @@ TARGET_ENABLE_QC_AV_ENHANCEMENTS := false
 TARGET_FWK_SUPPORTS_AV_VALUEADDS := true
 TARGET_USES_AOSP_FOR_WLAN := true
 ENABLE_CAR_POWER_MANAGER := true
-TARGET_USES_GAS := true
+#TARGET_USES_GAS := true
 
 
 # Dynamic-partition enabled by default
