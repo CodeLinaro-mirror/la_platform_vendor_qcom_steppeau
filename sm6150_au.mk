f0bc0035a4cfbf25458027d2d8c0f16d927e044c
Gangadhar Kavalastramath <quic_gkavalas@quicinc.com>
2021-10-11 17:46:01 +0530

    Adding pci-msm-drv and cnss to vendor modprobe list
    To improve android boot time compiling pci-msm-drv and cnss drivers
    as dlkm and adding them to vendor modprobe list. This change improves
    boot time by 50ms

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -179,6 +179,12 @@ AUDIO_DLKM += audio_native.ko
 AUDIO_DLKM += audio_machine_talos.ko
 PRODUCT_PACKAGES += $(AUDIO_DLKM)
 
+PCIE_DLKM := pci_msm_drv
+PRODUCT_PACKAGES += $(PCIE_DLKM)
+
+CNSS_DLKM := cnss2
+PRODUCT_PACKAGES += $(CNSS_DLKM)
+
 # HS-I2S DLKM
 PRODUCT_PACKAGES += hsi2s.ko
 # HS-I2S test app
