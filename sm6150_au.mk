d3b82a6d1449d9c8227bb3a41ecd61a264b4d9a9
Gopala Krishna Nuthaki <quic_gnuthaki@quicinc.com>
2021-11-17 16:37:41 +0530

    sm6150_au: Remove thermal hal mock service
    Remove thermal-hal mock service.
    
    Change-Id: Icbd93d1f4978b1e3513cb61977d771b00ae37751
    Signed-off-by: Gopala Krishna Nuthaki <quic_gnuthaki@quicinc.com>

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -344,7 +344,6 @@ PRODUCT_PACKAGES += canflasher \
                     android.hardware.automotive.vehicle@2.0-manager-lib-shared
 
 PRODUCT_PACKAGES += android.hardware.dumpstate@1.1-service.example \
-                    android.hardware.thermal@2.0-service.mock \
 
 PRODUCT_PACKAGES += android.hardware.health@2.1-service \
                     android.hardware.health@2.1-impl \
