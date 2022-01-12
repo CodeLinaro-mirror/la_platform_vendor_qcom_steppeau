1ebec5fefdf054284865057fe3b3729fb510f085
Santosh Dronamraju <quic_sdronamr@quicinc.com>
2022-01-12 16:14:23 +0530

    sm6150_au:Fix for cts keystore Assumption failures
    Add attest key xml file to vendor path so that it will be available
    in pm list and identifies that attest key feature has support.
    
    Change-Id: I115f2e06dc78cf5c58749e77360ba540229fef49

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -182,6 +182,7 @@ endif #TARGET_ENABLE_QC_AV_ENHANCEMENTS
 PRODUCT_COPY_FILES += device/qcom/$(MSMSTEPPE)_au/hiber.sh:$(TARGET_COPY_OUT_VENDOR)/bin/hiber.sh
 
 PRODUCT_COPY_FILES += hardware/interfaces/security/keymint/aidl/default/android.hardware.hardware_keystore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.hardware_keystore.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
 PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
 
 #Audio DLKM
