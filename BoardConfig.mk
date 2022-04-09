532a00ad2141070871c4e438ab876f21cbdac34e
Dheeraj Kumar Chaudhary <quic_dheech@quicinc.com>
2022-04-09 15:59:49 +0530

    sm6150_au: Disable kpti from command line
    Disable kpti from command line as it KERNEL CONFIG enables
    KPTI mitigation flag.
    
    Change-Id: I2e856c11e73bc89b1a082fdc50183be446095d72
    Signed-off-by: Dheeraj Kumar Chaudhary <quic_dheech@quicinc.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,7 +195,7 @@ TARGET_USES_NEW_ION_API :=true
 TARGET_USES_QCOM_BSP := false
 TARGET_USES_DRM_PP := true
 #BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 firmware_class.path=/vendor/firmware_mnt/image earlycon=msm_geni_serial,0x880000 nokaslr
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image earlycon=msm_geni_serial,0x880000 androidboot.selinux=enforcing androidboot.recover_usb=1 androidboot.usbcontroller=a600000.dwc3 hibernate=nocompress noswap_randomize
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image earlycon=msm_geni_serial,0x880000 androidboot.selinux=enforcing androidboot.recover_usb=1 androidboot.usbcontroller=a600000.dwc3 hibernate=nocompress noswap_randomize kpti=0
 MEMORY_HOTPLUG_2G := false
 ifeq ($(MEMORY_HOTPLUG_2G),true)
 BOARD_KERNEL_CMDLINE += no_hotplug_area=0x80000000,0x40000000 dyn_memhotplug
