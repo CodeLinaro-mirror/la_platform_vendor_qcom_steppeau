6bc6ca448a321c53729f249ac7460c3d6594d8d8
Tabassum Tabassum <quic_ttabassu@quicinc.com>
2022-09-22 17:10:57 +0530

    sm6150_au : updating shipping and sdk level
    Change-Id: I3d29191b7ad4a5bbe6c405be1c3894917fd79ae5

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -381,8 +381,8 @@ TARGET_MOUNT_POINTS_SYMLINKS := false
 PRODUCT_PACKAGES += libnbaio
 
 PRODUCT_PACKAGES += qcar-gsi.avbpubkey
-SHIPPING_API_LEVEL := 31
-PRODUCT_SHIPPING_API_LEVEL := 31
+SHIPPING_API_LEVEL := 32
+PRODUCT_SHIPPING_API_LEVEL := 32
 
 PRODUCT_PACKAGES += android.hardware.neuralnetworks@1.0.vendor \
                     android.hardware.neuralnetworks@1.1.vendor \
