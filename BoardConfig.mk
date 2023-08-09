a26a87464b4a269959b3153620899e1fd6a2fd07
Subhadra Jagadeesan <quic_c_subhja@quicinc.com>
2023-08-09 12:49:55 +0530

    msmsteppe_au: enable 64bit audio service
    Add change to enable 64bit version of audio service.
    
    Change-Id: Ie6de69295860ce5702d0bf177647d4cc7f42f70d
    Signed-off-by: Subhadra Jagadeesan <quic_c_subhja@quicinc.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -232,6 +232,11 @@ SOONG_CONFIG_NAMESPACES += qtiwifi
 SOONG_CONFIG_qtiwifi += automobile
 SOONG_CONFIG_qtiwifi_automobile := true
 
+#enable 64bit audioservice
+SOONG_CONFIG_NAMESPACES += android_hardware_audio
+SOONG_CONFIG_android_hardware_audio += run_64bit
+SOONG_CONFIG_android_hardware_audio_run_64bit := true
+
 #----------------------------------------------------------------------
 # wlan specific
 #----------------------------------------------------------------------
