59968c5642a2402cd7100a7a85f6ef4c81444244
Darshankumar Jagdishchandra Thakkar <quic_djagdish@quicinc.com>
2023-12-28 14:43:40 +0530

    sm6150_au: Add Hibernation Script
    To support Hibernation feature we need
    Hibernation script for sdmsteppeauto.
    
    Change-Id: I2a5c7732beb291bcfc8fa927d885c0821cec059c
    Signed-off-by: Darshankumar Jagdishchandra Thakkar <quic_djagdish@quicinc.com>

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -289,6 +289,7 @@ endif #TARGET_ENABLE_QC_AV_ENHANCEMENTS
 
 #Hibernation Script
 PRODUCT_COPY_FILES += device/qcom/$(MSMSTEPPE)_au/hiber.sh:$(TARGET_COPY_OUT_VENDOR)/bin/hiber.sh
+PRODUCT_COPY_FILES += device/qcom/$(MSMSTEPPE)_au/hiber_restore.sh:$(TARGET_COPY_OUT_VENDOR)/bin/hiber_restore.sh
 
 PRODUCT_COPY_FILES += hardware/interfaces/security/keymint/aidl/default/android.hardware.hardware_keystore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.hardware_keystore.xml
 PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
