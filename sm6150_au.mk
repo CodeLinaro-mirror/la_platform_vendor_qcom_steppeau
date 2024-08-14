c10c150cec2e782eaae63199b9cb4efcc286b601
Sindhu Kanathur <quic_skanathu@quicinc.com>
2024-08-14 13:23:09 +0530

    sm6150_au: Add default BroadcastRadio HAL
    Add default broadcastradio service to fix following runtime errors.
    E AndroidRuntime: java.lang.RuntimeException:
    Unable to create service com.android.car.radio.service.RadioAppService:
    java.lang.NullPointerException: RadioManager could not be loaded
    
    Change-Id: I4369df78ca5c37f2a50afc246e78de6b08b8bffc
    Signed-off-by: Sindhu Kanathur <quic_skanathu@quicinc.com>
    (cherry picked from commit cc0b27564414e0c3c50c189632d833d77cc54653)

--- sm6150_au.mk
+++ sm6150_au.mk
@@ -376,6 +376,13 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := vendor/qcom/opensource/core-utils/
 # Enable Scoped Storage related
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# BroadcastRadio
+PRODUCT_PACKAGES += \
+    android.hardware.broadcastradio-service.default
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.broadcastradio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.broadcastradio.xml \
+
 #ANT+ stack
 #PRODUCT_PACKAGES += \
 #    AntHalService \
