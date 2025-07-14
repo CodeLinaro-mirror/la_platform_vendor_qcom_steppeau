03396a7b934c20c02ca858276a5bf8c9d68ba9d5
Manjunatha Madana <c_mamanj@qti.qualcomm.com>
2025-07-14 10:45:21 +0530

    sm6150_au: Correct the path for device-vendor.mk
    Change-Id: I53eaec8cc92592e9f3ce10b392636c5064ca343d
    Signed-off-by: Manjunatha Madana <c_mamanj@qti.qualcomm.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,8 +7,8 @@
 TARGET_KERNEL_DLKM_DISABLE := false
 
 #We are resetting BOARD_VENDOR_KERNEL_MODULES due to BoardConfig.mk invoked twice
-#   1. From vendor/qcom/proprietary/common/config/device-vendor.mk
-#   2. From build/make/core/board_config.mk
+# 1. From $(QCPATH)/common/config/device-vendor.mk
+# 2. From build/make/core/board_config.mk
 #which impacts duplicates found in vendor_dlkm partition while building image
 ifneq ( ,$(filter Baklava 16,$(PLATFORM_VERSION)))
 BOARD_VENDOR_KERNEL_MODULES :=
@@ -347,8 +347,8 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 ENABLE_CAMERA_SERVICE := true
 
 #We are sorting BOARD_VENDOR_KERNEL_MODULES due to BoardConfig.mk invoked twice
-#   1. From vendor/qcom/proprietary/common/config/device-vendor.mk
-#   2. From build/make/core/board_config.mk
+# 1. From $(QCPATH)/common/config/device-vendor.mk
+# 2. From build/make/core/board_config.mk
 #which impacts duplicates found in vendor_dlkm partition while building image
 ifneq ( ,$(filter Baklava 16,$(PLATFORM_VERSION)))
 BOARD_VENDOR_KERNEL_MODULES := $(sort $(BOARD_VENDOR_KERNEL_MODULES))
