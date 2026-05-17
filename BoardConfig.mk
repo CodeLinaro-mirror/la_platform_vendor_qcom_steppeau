3752ec699f1568944f15bbb6bf7e39d060a39860
Rajesh RN <rajeshra@qti.qualcomm.com>
2026-05-17 22:54:54 +0530

    sm6150_au: Remove vbmeta_system from AB_OTA_PARTITIONS
    Remove vbmeta_system from AB_OTA_PARTITIONS
    
    Change-Id: I00624260a500b2d41d78ca51175f9b2fcdcbb7db
    Signed-off-by: Rajesh RN <rajeshra@qti.qualcomm.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,7 +150,7 @@ AB_OTA_UPDATER := true
 ifeq ($(TARGET_SINGLE_TREE), true)
   AB_OTA_PARTITIONS ?= init_boot vendor vendor_dlkm system_dlkm vbmeta vendor_boot boot dtbo vbmeta_system system system_ext product
 else
-  AB_OTA_PARTITIONS ?= init_boot vendor vendor_dlkm system_dlkm vbmeta vendor_boot boot dtbo vbmeta_system
+  AB_OTA_PARTITIONS ?= init_boot vendor vendor_dlkm system_dlkm vbmeta vendor_boot boot dtbo
 endif
 BOARD_USES_METADATA_PARTITION := true
 
